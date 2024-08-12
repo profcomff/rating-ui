95a3c3ab9aa3ab4c0155f2f761a579d15c8908d9
default <batuev.io18@physics.msu.ru>
2024-08-12 18:17:23 +0000

    saved all files

--- src/pages/MainPage.vue
+++ src/pages/MainPage.vue
@@ -1,55 +1,106 @@
 <script setup lang="ts">
-import { userdataUserApi } from '../api';
 import { useProfileStore } from '../store';
-import { onMounted, ref } from 'vue';
+import { createClient, setupAuth } from '@profcomff/api-uilib';
+import { router } from '../router';
 
 const profileStore = useProfileStore();
 
-const showUserWarn = ref(false);
+const apiClient = createClient('https://api.profcomff.com');
+setupAuth(profileStore.token);
 
-onMounted(async () => {
-	// Get username
-	if (!profileStore.full_name && profileStore.id) {
-		const resp = await userdataUserApi.getById(profileStore.id);
-		if (resp.status != 200) {
-			showUserWarn.value = true;
-			return;
-		}
-		const data = resp.data;
-		if (!data || !data.items) {
-			showUserWarn.value = true;
-			return;
-		}
-		const nameItem = data.items.find(item => {
-			return item.category == 'Личная информация' && item.param == 'Полное имя';
-		});
-		if (!nameItem) {
-			showUserWarn.value = true;
-			return;
-		}
-		profileStore.full_name = nameItem?.value ?? null;
-	}
-});
+const lecturers = await apiClient.GET('/timetable/lecturer/');
+const lecturersPhotos = lecturers.data?.items.map(
+	(item: (typeof lecturers.data.items)[0]) =>
+		`${import.meta.env.VITE_AUTH_API_BASE_URL}${item['avatar_link']}` ?? '../assets/example.jpg',
+);
 
-const location = document.location.origin + '/docs/';
+function toLecturerPage(id: number) {
+	router.push({ path: 'teacher-page', query: { lecturer_id: id } });
+}
+
+function loadMoreLectors() {
+	console.log('Загрузить еще лекторов');
+}
 </script>
 
 <template>
-	<div>
-		<h1 class="text-h1">
-			Привет<span v-if="profileStore.full_name">, {{ profileStore.full_name }}</span
-			>!
-		</h1>
-		<p v-if="!profileStore.full_name">
-			Не удалось получить твое имя из
-			<a href="https://api.profcomff.com/?urls.primaryName=userdata">Userdata API</a>
-			=(
-		</p>
-		<p>Твой id: {{ profileStore.id }}</p>
-		<div>
-			<p>
-				Документация к этому коду находится по адресу <a href="/docs/">{{ location }}</a>
-			</p>
-		</div>
-	</div>
+	<v-container v-if="lecturers">
+		<v-card max-width="750px">
+			<v-text-field
+				prepend-inner-icon="mdi-magnify"
+				class="mx-2 py-2"
+				max-width="750px"
+				variant="outlined"
+				density="compact"
+				placeholder="Найдите преподавателя"
+				clearable
+				hide-details="auto"
+				single-line
+				rounded="pill"
+			/>
+			<div class="d-flex align-center mb-2">
+				<v-sheet prepend-icon="mdn-magnify" class="mx-2 px-2 text-body-2">фильтры</v-sheet>
+				<v-divider vertical />
+				<v-slide-group>
+					<v-btn class="ma-1 text-body-2" rounded variant="tonal" density="compact" text="по рейтингу" />
+					<v-btn class="ma-1 text-body-2" rounded variant="tonal" density="compact" text="по доброте" />
+					<v-btn class="ma-1 text-body-2" rounded variant="tonal" density="compact" text="по доброте" />
+					<v-btn class="ma-1 text-body-2" rounded variant="tonal" density="compact" text="по понятности" />
+				</v-slide-group>
+			</div>
+		</v-card>
+		<v-card
+			v-for="(lecturer, idx) in lecturers.data?.items"
+			:key="idx"
+			class="my-2 py-2"
+			variant="elevated"
+			@click="toLecturerPage(lecturer.id)"
+		>
+			<template #prepend>
+				<v-avatar size="100" rounded="rounded" class="pr-2 mr-2">
+					<v-img :src="lecturersPhotos[idx]"></v-img>
+				</v-avatar>
+			</template>
+			<template #title>
+				<v-sheet class="text-h4">{{ lecturer.last_name }}</v-sheet>
+			</template>
+			<template #subtitle>
+				<v-sheet class="text-body-1">{{ lecturer.first_name }} {{ lecturer.middle_name }}</v-sheet>
+				<div class="text-body-2">
+					<v-sheet>предмет: английский</v-sheet>
+					<v-sheet>отзывов: 123</v-sheet>
+					<div class="d-flex">
+						<v-sheet>общая оценка: +100</v-sheet>
+						<v-spacer />
+						<v-btn
+							class="text-caption"
+							variant="tonal"
+							color="primary"
+							size="x-small"
+							justify="right"
+							text="+отзыв"
+						/>
+					</div>
+				</div>
+			</template>
+		</v-card>
+		<v-container>
+			<v-btn class="d-flex mx-auto" text="Загрузить еще" @click="loadMoreLectors" />
+		</v-container>
+	</v-container>
 </template>
+
+<style>
+.toolbar {
+	display: table;
+}
+
+.contact a {
+	display: table;
+}
+
+.main {
+	align-items: center;
+	max-width: 750px;
+}
+</style>
---
+++ src/pages/TeacherPage.vue
@@ -0,0 +1,122 @@
+<script setup lang="ts">
+import Markdown from '../components/MarkdownRenderer.vue';
+import { createClient, setupAuth } from '@profcomff/api-uilib';
+import { useProfileStore } from '../store';
+import { useRouter } from 'vue-router';
+
+const router = useRouter();
+const profileStore = useProfileStore();
+
+const apiClient = createClient('https://api.profcomff.com');
+setupAuth(profileStore.token);
+
+const url = new URL(document.location.toString());
+const lecturerId = url.searchParams.get('lecturer_id');
+
+console.log(lecturerId);
+
+const lecturer = await apiClient.GET(`/timetable/lecturer/{id}`, {
+	params: {
+		path: {
+			id: Number(lecturerId),
+		},
+	},
+});
+
+let howKind: number = 10;
+let howEasy: number = 35;
+let howUnderstandable: number = 60;
+
+const lecturerLastName = lecturer.data['last_name'];
+const lecturerFirstName = lecturer.data['first_name'];
+const lecturerMiddleName = lecturer.data['middle_name'];
+
+const lecturerPhoto =
+	`${import.meta.env.VITE_AUTH_API_BASE_URL}${lecturer.data['avatar_link']}` ?? '../assets/example.jpg';
+const lecturerInfo = lecturer.data['description'] ?? 'Информации нет';
+const screenWidth = window.innerWidth;
+</script>
+
+<template>
+	<div v-if="lecturer" class="main">
+		<v-btn text="Назад к поиску" @click="router.push('/')"></v-btn>
+		<v-card class="container" width="100%" variant="text">
+			<template #prepend>
+				<v-avatar size="120">
+					<v-img :src="lecturerPhoto"></v-img>
+				</v-avatar>
+			</template>
+			<template #title>
+				<v-sheet class="text-h4">{{ lecturerLastName }}</v-sheet>
+			</template>
+			<template #subtitle>
+				<v-sheet class="text-body-1">{{ lecturerFirstName }} {{ lecturerMiddleName }}</v-sheet>
+				<div class="contact text-body-2">
+					<a href="mailto:ivanovii@example.com">a@example.com</a>
+					<a href="tel:+7(999)999-99-99">+7(999)999-99-99</a>
+				</div>
+			</template>
+		</v-card>
+		<v-container>
+			<v-progress-circular class="rating-bar text-body-2" :size="130" :width="8" :model-value="howKind">
+				Доброта
+			</v-progress-circular>
+			<v-progress-circular class="rating-bar text-body-2" :size="130" :width="8" :model-value="howEasy">
+				Халявность
+			</v-progress-circular>
+			<v-progress-circular class="rating-bar text-body-2" :size="130" :width="8" :model-value="howUnderstandable">
+				Понятность
+			</v-progress-circular>
+		</v-container>
+		<v-container max-width="750px">
+			<v-row justify="center" no-gutters>
+				<v-col cols="md-2">
+					<v-card class="pr-1 mr-1" height="100px" variant="tonal" min-width="100px">
+						<template #prepend>
+							<v-icon icon="$vuetify"></v-icon>
+						</template>
+						<template #title>+100</template>
+						<template #text>123 отзыва</template>
+					</v-card>
+				</v-col>
+				<v-col cols="xs-2">
+					<v-card class="pl-1 ml-1" height="100px" color="#eee" variant="flat">
+						<v-card-item :class="screenWidth < 600 ? 'text-caption' : 'text-body-1'">
+							<p>В рейтинге учитываются лишь отзывы, оставленные за последние 3 года</p>
+						</v-card-item>
+					</v-card>
+				</v-col>
+			</v-row>
+		</v-container>
+		<v-container class="">
+			<Markdown :text="lecturerInfo" />
+		</v-container>
+	</div>
+</template>
+
+<style>
+.contact a {
+	display: table;
+}
+
+.v-container {
+	margin: auto;
+	max-width: 750px;
+	display: flex;
+	justify-content: center;
+}
+
+.main {
+	align-items: center;
+}
+
+.v-col {
+	margin: auto;
+}
+
+.rating-bar {
+	color: #999;
+	margin: auto;
+	font-size: small;
+}
+</style>
