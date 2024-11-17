9b4b8c93a64d135b9e41b743e5f4f8b436d99337
default <batuev.io18@physics.msu.ru>
2024-11-17 17:18:44 +0000

    added LocalStorage and adapted profileStore

--- src/store/profileStore.ts
+++ src/store/profileStore.ts
@@ -1,10 +1,11 @@
 import { defineStore } from 'pinia';
 import { ref } from 'vue';
+import { LocalStorage, LocalStorageItem } from '../models/LocalStorage';
 
 export const useProfileStore = defineStore('profile', () => {
 	const id = ref<number | null>(null);
 	const email = ref<string | null>(null);
-	const token = ref<string | undefined>();
+	const token = ref<string | null>();
 	const groups = ref<number[] | null>(null);
 	const indirectGroups = ref<number[] | null>(null);
 	const userScopes = ref<string[] | null>(null);
@@ -14,17 +15,30 @@ export const useProfileStore = defineStore('profile', () => {
 
 	const fromUrl = () => {
 		const url = new URL(document.location.toString());
-		const currToken = url.searchParams.get('token');
-		const currId = url.searchParams.get('user_id');
-		const currScopes = url.searchParams.get('scopes');
-		if (currToken) {
-			token.value = currToken;
+
+		const localToken = LocalStorage.get(LocalStorageItem.Token);
+		const urlToken = url.searchParams.get('token');
+
+		if (urlToken === undefined) {
+			token.value = localToken;
+		} else {
+			token.value = urlToken;
+			LocalStorage.set(LocalStorageItem.Token, urlToken);
 		}
+
+		const localScopes = LocalStorage.getObject<string[]>(LocalStorageItem.TokenScopes);
+		const urlScopes = url.searchParams.get('scopes')?.split(',');
+
+		if (urlScopes === undefined) {
+			sessionScopes.value = localScopes;
+		} else {
+			sessionScopes.value = urlScopes;
+			LocalStorage.set(LocalStorageItem.TokenScopes, urlScopes);
+		}
+
+		const currId = url.searchParams.get('user_id') ?? LocalStorage.get(LocalStorageItem.UserId) ?? undefined;
 		if (currId) {
 			id.value = +currId;
-		}
-		if (currScopes) {
-			sessionScopes.value = currScopes.split(',');
 		}
 	};
 
