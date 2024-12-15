ea0fb94342de08fad02509797b01cda1f12ea1b6
Батуев Илья Олегович <batuevio@it.profcomff.com>
2024-12-15 16:58:24 +0000

    fixed

--- src/components/AdminReviewCard.vue
+++ src/components/AdminReviewCard.vue
@@ -4,9 +4,7 @@
       <div class="text-h6 font-weight-bold">{{ lecturerName }}</div>
     </template>
     <template #subtitle>
-      <div class="text-h7">
-        {{ formattedDate }} | {{ "Анонимный отзыв" }}
-      </div>
+      <div class="text-h7">{{ formattedDate }} | {{ "Анонимный отзыв" }}</div>
     </template>
     <template #text>
       <p class="text-subtitle-2">{{ comment.raw.text }}</p>
@@ -33,11 +31,10 @@
 </template>
 
 <script setup lang="ts">
-import { ref, onMounted } from 'vue';
+import { ref, onMounted } from "vue";
 import apiClient from "@/api";
 import { ToastType } from "@/models";
 import { useToastStore } from "@/store/toastStore";
-import { format } from 'date-fns';
 
 const props = defineProps({
   comment: { type: Object, required: true },
@@ -46,84 +43,66 @@ const props = defineProps({
 const emit = defineEmits(["decided"]);
 
 const toastStore = useToastStore();
-const lecturerName = ref('');
-const formattedDate = ref('');
+const lecturerName = ref("");
+const formattedDate = ref("");
 
 onMounted(async () => {
-  lecturerName.value = await getLecturerName(props.comment.raw.lecturer_id);
-  formattedDate.value = format(new Date(props.comment.raw.create_ts), 'dd.MM.yyyy HH:mm');
+  const lecturer = await getLecturerName(props.comment.raw.lecturer_id);
+  lecturerName.value = lecturer
+    ? [lecturer.last_name, lecturer.first_name, lecturer.middle_name].join(" ")
+    : "Неизвестный лектор";
+  formattedDate.value = Intl.DateTimeFormat().format(
+    Date.parse(props.comment.raw.create_ts),
+  );
 });
 
 async function getLecturerName(lecturerId: string) {
-  const { response } = await apiClient.GET(`/lecturers/${lecturerId}`);
-  if (response.ok) {
-    return response.data.full_name;
-  } else {
-    toastStore.push({
-      title: "Ошибка",
-      type: ToastType.Error,
-      description: "Не удалось получить имя лектора",
-    });
-    return 'Неизвестный лектор';
-  }
+  const { data } = await apiClient.GET("/rating/lecturer/{id}", {
+    params: { path: { id: Number(lecturerId) } },
+  });
+  return data;
 }
 
 async function approveComment(id: string) {
-  try {
-    const { response } = await apiClient.PATCH("/rating/comment/{uuid}", {
-      params: {
-        path: { uuid: id },
-        query: { review_status: "approved" },
-      },
-    });
-    if (response.ok) {
-      toastStore.push({
-        title: "Отзыв одобрен",
-        type: ToastType.Info,
-      });
-      emit("decided");
-    } else {
-      toastStore.push({
-        title: "Что-то не так",
-        type: ToastType.Error,
-        description: response.statusText,
-      });
-    }
-  } catch (error) {
+  const { response } = await apiClient.PATCH("/rating/comment/{uuid}", {
+    params: {
+      path: { uuid: id },
+      query: { review_status: "approved" },
+    },
+  });
+  if (response.ok) {
     toastStore.push({
-      title: "Ошибка",
+      title: "Отзыв одобрен",
+      type: ToastType.Info,
+    });
+    emit("decided");
+  } else {
+    toastStore.push({
+      title: "Что-то пошло не так при одобрении отзыва",
       type: ToastType.Error,
-      description: "Произошла ошибка при одобрении отзыва",
+      description: response.statusText,
     });
   }
 }
 
 async function dismissComment(id: string) {
-  try {
-    const { response } = await apiClient.PATCH("/rating/comment/{uuid}", {
-      params: {
-        path: { uuid: id },
-        query: { review_status: "dismissed" },
-      },
-    });
-    if (response.ok) {
-      toastStore.push({
-        title: "Отзыв отклонен",
-        type: ToastType.Info,
-      });
-      emit("decided");
-    } else {
-      toastStore.push({
-        title: "Что-то не так",
-        type: ToastType.Error,
-        description: response.statusText,
-      });
-    }
-  } catch (error) {
+  const { response } = await apiClient.PATCH("/rating/comment/{uuid}", {
+    params: {
+      path: { uuid: id },
+      query: { review_status: "dismissed" },
+    },
+  });
+  if (response.ok) {
     toastStore.push({
-      title: "Ошибка",
+      title: "Отзыв отклонен",
+      type: ToastType.Info,
+    });
+    emit("decided");
+  } else {
+    toastStore.push({
+      title: "Что-то пошло не так при отклонении отзыва",
       type: ToastType.Error,
-      description: "Произошла ошибка при отклонении отзыва",
+      description: response.statusText,
     });
   }
 }
